d3fb48e69206adbb99a26846b7d377fb61c6f497
Alvarado08 <jorgealvarado0238@gmail.com>
2025-10-09 21:28:15 -0500

    AppBar
    Worked on AppBar layout with imported Poppins font and svgs with flutter_svg package. Wrapped Container widget with GestureDetector and extracted AppBar as a method for better code maintainability and readability. Learned about the action widget.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(appBar: appBar());
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: const Text(
+        'Breakfast',
+        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+      ),
+      centerTitle: true,
+      backgroundColor: Colors.white,
+      elevation: 0.0,
+      leading: GestureDetector(
+        onTap: () {},
+        child: Container(
+          margin: EdgeInsets.all(10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Color(0xffe8f0fe),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: SvgPicture.asset('assets/icons/Arrow - Left 2.svg'),
+        ),
+      ),
+      actions: [
+        GestureDetector(
+          onTap: () {},
+          child: Container(
+            margin: EdgeInsets.all(10),
+            alignment: Alignment.center,
+            width: 37,
+            decoration: BoxDecoration(
+              color: Color(0xffe8f0fe),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: SvgPicture.asset('assets/icons/dots.svg'),
+          ),
+        ),
+      ],
+    );
   }
 }
